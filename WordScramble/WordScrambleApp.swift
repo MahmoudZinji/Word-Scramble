20a2a1857fa6712de72682f86b2ef4b079f4b190
Mahmoud Zinji <mahmoud.zinji@montymobile.com>
2021-06-11 09:27:36 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by User on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
